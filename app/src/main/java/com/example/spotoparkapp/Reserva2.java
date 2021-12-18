b77dbaa741fe1c28c348cbacecf6d227485cf934
Pedro Santos <pedrojgmsantos@gmail.com>
2021-12-18 21:00:30 +0000

    commit

--- app/src/main/java/com/example/spotoparkapp/Reserva2.java
+++ app/src/main/java/com/example/spotoparkapp/Reserva2.java
@@ -2,13 +2,67 @@ package com.example.spotoparkapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class Reserva2 extends AppCompatActivity {
+
+    EditText text;
+    Button button;
+    ImageView QRcode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reserva2);
+
+        text = findViewById(R.id.inputText);
+        button = findViewById(R.id.btQR);
+        QRcode = findViewById(R.id.imageQR);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Obter o valor através do edit text
+                String sText = text.getText().toString().trim();
+                //Inicializar a multi format writer
+                MultiFormatWriter writer = new MultiFormatWriter();
+                try {
+                    //Inicializar a bit matrix
+                    BitMatrix matrix = writer.encode(sText, BarcodeFormat.QR_CODE, 400, 400);
+                    //inicializar a barcode encoder
+                    BarcodeEncoder encoder = new BarcodeEncoder();
+                    //Inicializar bitmap
+                    Bitmap bitmap = encoder.createBitmap(matrix);
+                    //Colocar o bitmap numa imageview
+                    QRcode.setImageBitmap(bitmap);
+                    //Inicializar o input manager
+                    InputMethodManager manager = (InputMethodManager)  getSystemService(
+                            Context.INPUT_METHOD_SERVICE
+                    );
+                    manager.hideSoftInputFromWindow(text.getApplicationWindowToken(), 0);
+                } catch (WriterException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
+
+    /*public void onCLickGoParque2(View v) {
+        Intent intent = new Intent(getApplicationContext(), Parque2.class);
+        startActivity(intent);
+    }*/
 }

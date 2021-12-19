b8faa5220ce8ab5199a8ad82ca2906d50f0a5851
Pedro Santos <pedrojgmsantos@gmail.com>
2021-12-19 01:25:40 +0000

    commit

--- app/src/main/java/com/example/spotoparkapp/Maps.java
+++ app/src/main/java/com/example/spotoparkapp/Maps.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
@@ -11,8 +12,10 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.SearchView;
 import android.widget.Toast;
 
@@ -22,6 +25,8 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 
+import com.example.spotoparkapp.databinding.ActivityMapsBinding;
+import com.example.spotoparkapp.directions.DirectionResponses;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -30,12 +35,22 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.android.PolyUtil;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Maps extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener, GoogleMap.OnInfoWindowClickListener  {
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public class Maps extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener, GoogleMap.OnInfoWindowClickListener, GoogleMap.OnMyLocationChangeListener  {
 
     GoogleMap mMap;
     MapView mapView;
@@ -43,21 +58,29 @@ public class Maps extends AppCompatActivity implements OnMapReadyCallback, Googl
     LocationListener locationListener;
     double lati, longi;
     SearchView searchView;
+    private LatLng USERLOCATION;
+    private ImageButton directions;
+    private Marker markerOne;
+    private Marker markerTwo;
+    private PolylineOptions polyline1;
+    private GoogleMap googleMap;
+    ActivityMapsBinding binding;
+    private ArrayList<Marker> markers;
+
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
 
         mapView = findViewById(R.id.mapView);
         mapView.getMapAsync(this);
         mapView.onCreate(savedInstanceState);
 
-
-
-
-
+        directions = findViewById(R.id.imageButton32);
+        directions = binding.imageButton32;
 
         // 1 - Criar o location Manager para ir buscar a localização do nosso dispositivo
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
@@ -129,24 +152,24 @@ public class Maps extends AppCompatActivity implements OnMapReadyCallback, Googl
         LatLng santos = new LatLng(38.7071236,-9.1525369);
         LatLng userLocation = new LatLng(lati, longi);
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(santos, 15));
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        mMap.setOnMyLocationChangeListener((GoogleMap.OnMyLocationChangeListener) Maps.this);
 
-        List<Marker> markers  = new ArrayList<>();
+        markers = new ArrayList<>();
 
         //When map is loaded
         LatLng parque1 = new LatLng(38.706984, -9.151735);
-        Marker markerOne = googleMap.addMarker(new MarkerOptions().position(parque1).title("Fazer Reserva 1").snippet("SpoToPark Parque 1")
+        markerOne = googleMap.addMarker(new MarkerOptions().position(parque1).title("Fazer Reserva 1").snippet("SpoToPark Parque 1")
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
 
 
         LatLng parque2 = new LatLng(38.708030, -9.147979);
-        Marker markerTwo = googleMap.addMarker(new MarkerOptions().position(parque2).title("Fazer Reserva 2").snippet("SpoToPark Parque 2")
+        markerTwo = googleMap.addMarker(new MarkerOptions().position(parque2).title("Fazer Reserva 2").snippet("SpoToPark Parque 2")
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
-
-        markerOne.setTag(1);
-        markerTwo.setTag(2);
 
         markers.add(markerOne);
         markers.add(markerTwo);
+
         this.mMap.setOnInfoWindowClickListener((GoogleMap.OnInfoWindowClickListener) this);
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
@@ -212,7 +235,95 @@ public class Maps extends AppCompatActivity implements OnMapReadyCallback, Googl
          }
      }
 
+    @Override
+    public void onInfoWindowClick(Marker marker) {
 
+        if (marker.getTitle().equals("Fazer Reserva 1")) {
+            Intent intent = new Intent(getApplicationContext(), Parque1.class);
+            startActivity(intent);
+        }else if (marker.getTitle().equals("Fazer Reserva 2")){
+            Intent intent = new Intent(getApplicationContext(), Parque2.class);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public boolean onMarkerClick(@NonNull Marker marker) {
+
+        //directions.setVisibility(View.VISIBLE);
+
+        for (int i = 0; i < markers.size(); i++){
+            markers.get(i).setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
+            if (markers.get(i).getId().equals(marker.getId())) {
+
+                LatLng location = markers.get(i).getPosition();
+            }
+        }
+        marker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+
+        directions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.e("USERLOCATION", ""+USERLOCATION);
+                String userlocationString = String.valueOf(USERLOCATION.latitude) + "," + String.valueOf(USERLOCATION.longitude);
+                String end = String.valueOf(marker.getPosition().latitude) + "," + String.valueOf(marker.getPosition().longitude);
+
+                ApiServices apiServices = RetrofitClient.apiServices(getApplicationContext());
+                apiServices.getDirection(userlocationString, end, getString(R.string.api_key))
+                        .enqueue(new Callback<DirectionResponses>() {
+
+                            @Override
+                            public void onResponse(Call<DirectionResponses> call, retrofit2.Response<DirectionResponses> response) {
+                                drawPolyline(response);
+                                Log.d("ok", response.message());
+                            }
+
+                            @Override
+                            public void onFailure(@NonNull Call<DirectionResponses> call, @NonNull Throwable t) {
+                                Log.e("error", t.getLocalizedMessage());
+                            }
+                        });
+            }
+        });
+
+        return false;
+    }
+
+    @Override
+    public void onMyLocationChange(@NonNull Location location) {
+        USERLOCATION = new LatLng(location.getLatitude(), location.getLongitude());
+        //Log.e("Sending updates", "" + USERLOCATION);
+    }
+
+    private void drawPolyline(@NonNull Response<DirectionResponses> response) {
+        if (response.body() != null) {
+            String shape = response.body().getRoutes().get(0).getOverviewPolyline().getPoints();
+            polyline1 = new PolylineOptions()
+                    .addAll(PolyUtil.decode(shape))
+                    .width(8f)
+                    .color(Color.RED);
+            googleMap.addPolyline(polyline1);
+        }
+    }
+
+    private interface ApiServices {
+        @GET("maps/api/directions/json")
+        Call<DirectionResponses> getDirection(@Query("origin") String origin,
+                                              @Query("destination") String destination,
+                                              @Query("key") String apiKey);
+    }
+
+    private static class RetrofitClient {
+        static ApiServices apiServices(Context context) {
+            Retrofit retrofit = new Retrofit.Builder()
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .baseUrl(context.getResources().getString(R.string.base_url))
+                    .build();
+
+            return retrofit.create(ApiServices.class);
+        }
+    }
 
     @Override
     protected void onStart() {
@@ -259,22 +370,5 @@ public class Maps extends AppCompatActivity implements OnMapReadyCallback, Googl
     public void onClickGoMenu(View v) {
         Intent intent = new Intent(getApplicationContext(), Menu.class);
         startActivity(intent);
-    }
-
-    @Override
-    public void onInfoWindowClick(Marker marker) {
-
-        if (marker.getTitle().equals("Fazer Reserva 1")) {
-            Intent intent = new Intent(getApplicationContext(), Parque1.class);
-            startActivity(intent);
-        }else if (marker.getTitle().equals("Fazer Reserva 2")){
-            Intent intent = new Intent(getApplicationContext(), Parque2.class);
-            startActivity(intent);
-        }
-    }
-
-    @Override
-    public boolean onMarkerClick(@NonNull Marker marker) {
-        return false;
     }
 }

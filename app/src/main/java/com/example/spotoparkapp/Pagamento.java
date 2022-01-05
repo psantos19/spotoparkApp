eb44ed8df22587b457087aea71ae8528638f1b99
Pedro Santos <pedrojgmsantos@gmail.com>
2022-01-05 23:15:12 +0000

    c

--- app/src/main/java/com/example/spotoparkapp/Pagamento.java
+++ app/src/main/java/com/example/spotoparkapp/Pagamento.java
@@ -1,21 +1,80 @@
 package com.example.spotoparkapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.braintreepayments.cardform.view.CardForm;
+
 public class Pagamento extends AppCompatActivity {
+
+    CardForm cardForm;
+    Button buy;
+    AlertDialog.Builder alertBuilder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagamento);
+
+        /*
+
+        CardForm cardForm = findViewById(R.id.cardForm);
+        Button buy = findViewById(R.id.butaoPagamento);
+        cardForm.cardRequired(true)
+                .expirationRequired(true)
+                .cvvRequired(true)
+                .postalCodeRequired(true)
+                .mobileNumberRequired(true)
+                .mobileNumberExplanation("SMS is required on this number")
+                .setup(Pagamento.this);
+        cardForm.getCvvEditText().setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_VARIATION_PASSWORD);
+        buy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (cardForm.isValid()) {
+                    alertBuilder = new AlertDialog.Builder(Pagamento.this);
+                    alertBuilder.setTitle("Confirm before purchase");
+                    alertBuilder.setMessage("Card number: " + cardForm.getCardNumber() + "\n" +
+                            "Card expiry date: " + cardForm.getExpirationDateEditText().getText().toString() + "\n" +
+                            "Card CVV: " + cardForm.getCvv() + "\n" +
+                            "Postal code: " + cardForm.getPostalCode() + "\n" +
+                            "Phone number: " + cardForm.getMobileNumber());
+                    alertBuilder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                            Toast.makeText(Pagamento.this, "Thank you for purchase", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                    alertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                        }
+                    });
+                    AlertDialog alertDialog = alertBuilder.create();
+                    alertDialog.show();
+                }else {
+                    Toast.makeText(Pagamento.this, "Please complete the form", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
+         */
     }
 
     public void onClickGoMenu(View v) {
         Intent intent = new Intent(getApplicationContext(), Menu.class);
         startActivity(intent);
     }
+
 }
